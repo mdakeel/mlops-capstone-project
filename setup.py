c6f17d8645442a139540575094b9d1896ae37cbf
Md Akeel <aakiltayyab@gmail.com>
2025-09-17 13:31:39 +0530

    Project Structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repo is an end to end mlops capstone project , In this will coverd all mlops technique',
+    author='Aakil Tayyab',
+    license='MIT',
+)
